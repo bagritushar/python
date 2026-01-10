1a103f3a019c5554a49b0b2f79e788448d171c7c
Tushar bagri <aryatushar179@gmail.com>
2026-01-10 22:11:31 +0530

    Chapter 6 completed - practice questions added

---
+++ CHAPTER 6 Conditional Expression/PRACTICE SET/07_problem.py
@@ -0,0 +1,10 @@
+# qus7 = Write a program to find out whether a given post is talking about “Harry” or not.
+
+post = input("Enter the post:")
+
+
+
+if ("Tushar" in post):
+    print("This post is talking about Tushar")
+else:
+    print("This post is not talking about Tushar")
