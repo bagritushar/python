7be4c438dd0b433cd3184cd9291d096826c70255
Tushar bagri <aryatushar179@gmail.com>
2026-01-13 21:06:07 +0530

    Chapter 8 started - First half completed - (Topic - Function) and next topic is #RECURSION

---
+++ CHAPTER 8  Functions & Recursions/02_Types_fuction.py
@@ -0,0 +1,23 @@
+
+
+
+# TYPES OF FUNCTIONS IN PYTHON
+
+# There are two types of functions in python:
+
+# • Built in functions (Already present in python)
+
+# • User defined functions (Defined by the user)
+
+# Examples of built in functions includes len(), print(), range() etc.
+
+# The func1() function we defined is an example of user defined function.
+
+
+
+#         FUNCTIONS WITH ARGUMENTS
+
+# A function can accept some value it can work with. We can put these values in the
+# parentheses.
+
+# A function can also return value as shown below:
---
+++ CHAPTER 8  Functions & Recursions/03_default_argument.py
@@ -0,0 +1,8 @@
+
+def goodDay(name, ending= "Thanks"):
+    print("Good Day," + name)
+    print(ending)                            #FUNCTIONS WITH ARGUMENTS
+
+
+goodDay("Tushar",)
+goodDay("Pragya",)
